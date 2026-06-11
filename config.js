bc1b076bf6afe4d7501e51ec89204b318c615480
14sail <xusail14@gmail.com>
2026-06-11 15:38:45 +0800

    fix: 補上 config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDEPwpz4a0jbCWZ_N-peJydCPVTHg4TU-A",
+    authDomain: "mood-calendar-sail.firebaseapp.com",
+    projectId: "mood-calendar-sail",
+    storageBucket: "mood-calendar-sail.firebasestorage.app",
+    messagingSenderId: "962446738343",
+    appId: "1:962446738343:web:418d197331ee97cd46ec82"
+};
